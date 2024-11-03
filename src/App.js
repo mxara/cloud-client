fa9abea80281680f24bf2451bfe53fe9a526f15e
maxim <maxara8976@gmail.com>
2024-11-03 19:11:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react"
+import axios from 'axios'
 
 function App() {
+  const [stocks, setStocks] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/api/stocks')
+      .then((response) => {
+        setStocks(response.data)
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      I am React
+
+      {
+        stocks ? (
+          <ul>
+            {stocks.map((stock) => {
+              return <li>{stock.title} - {stock.price}</li>
+            })}
+          </ul>
+        ) : ""}
     </div>
   );
 }
